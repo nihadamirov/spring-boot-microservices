a044a05541d237f4b4559e01ab553319a1b971b6
Nihad <nihadamirov98@gmail.com>
2024-07-23 23:45:48 +0400

    init file

---
+++ src/main/java/com/springbootmicroservices/SpringBootMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.springbootmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMicroservicesApplication.class, args);
+    }
+
+}
